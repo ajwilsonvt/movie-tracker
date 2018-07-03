4ed2215cc82c7a610e21194c98c5567a778d4de4
aaron-wilson <aaron-wilson@users.noreply.github.com>
2018-07-03 06:05:24 -0400

    resolve npm package errors

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -8,37 +8,29 @@ import Welcome from './Welcome';
 import MediaLists from './MediaLists';
 import MediaList from './MediaList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+const App = () => (
+  <div>
+    <Router>
       <div>
-        <Router>
-          <div>
-            <header>
-              <Link to="/movietracker" className="logo">
-                <h1 className="thin">M</h1>
-              </Link>
-              <nav>
-                <Link to="/movietracker/medialists">Media Lists</Link>
-              </nav>
-            </header>
-            <Route exact path="/movietracker" component={Welcome} />
-            <Route exact path="/movietracker/medialists" component={MediaLists} />
-            <Route exact path="/movietracker/medialists/:mediaListId" component={MediaList} />
-          </div>
-        </Router>
-        <a href="/profile">
-          <button className="btn btn-info exit-btn">
-            Back to Profile
-          </button>
-        </a>
+        <header>
+          <Link to="/movietracker" className="logo">
+            <h1 className="thin">M</h1>
+          </Link>
+          <nav>
+            <Link to="/movietracker/medialists">Media Lists</Link>
+          </nav>
+        </header>
+        <Route exact path="/movietracker" component={Welcome} />
+        <Route exact path="/movietracker/medialists" component={MediaLists} />
+        <Route exact path="/movietracker/medialists/:mediaListId" component={MediaList} />
       </div>
-    );
-  }
-}
+    </Router>
+    <a href="/profile">
+      <button className="btn btn-info exit-btn">
+        Back to Profile
+      </button>
+    </a>
+  </div>
+);
 
 export default App;
